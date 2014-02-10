d170cda61c327a659e20a07cd46ab6ee09d63ac1
xbsd <xbsd@yahoo.com>
2014-02-09 20:08:28 -0500

    Create Unix Tips.sh

---
+++ Unix Tips.sh
@@ -0,0 +1,8 @@
+# At terminal, run,
+sudo easy_install coursera-dl
+ 
+# Run coursera-dl, substitute id and password, for eg.,
+coursera-dl -u USERNAME -p PASSWORD -d DOWNLOADPATH COURSEID
+ 
+# If your Coursera id is john@abc.com, password is abcd123, Download Folder is /tmp/ and the course is nlp, run
+coursera-dl -u john@abc.com -p abcd123 -d /tmp/ nlp
