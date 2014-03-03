78a52196470e3079f1fed7912f80b7111e9e80c9
xbsd <xbsd@yahoo.com>
2014-03-03 11:57:24 -0500

    Update Tips.sh

--- Tips.sh
+++ Tips.sh
@@ -3,3 +3,12 @@ for name in *\ *; do mv -v "$name" "${name// /}"; done
 
 # Change to lowercase
 cat … | tr '[:upper:]' '[:lower:]'
+
+# To remove newlines, line breaks / line feeds in VI
+
+If you are on the first line, pressing (upper case) J will join that line and the next line together, 
+removing the newline. You can also combine this with a count, so pressing 3J will combine all 3 lines together.
+
+# To remove blank lines in vi
+:g/^$/d
+:g will execute a command on lines which match a regex. The regex is 'blank line' and the command is :d (delete)
